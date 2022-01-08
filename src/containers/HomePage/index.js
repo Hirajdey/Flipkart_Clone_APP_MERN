e73d72cd3f7cbf204b84bc4504c10caf202b1b83
Hiraj Dey <deyhiraj@gmail.com>
2022-01-08 20:22:46 +0530

    frontEnd code

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+/**
+* @author
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+    <div>
+        <Header/>
+    </div>
+   )
+
+}
+
+export default HomePage;
+
