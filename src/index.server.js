1a5ebb660b81900cf2c6022380e49d5f6d29d82d
Hiraj Dey <deyhiraj@gmail.com>
2022-01-08 20:11:00 +0530

    front-end

---
+++ src/index.server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+//routes
+const userRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin/auth');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
+const initialDataRoutes = require('./routes/admin/initialData');
+
+
+//enviroment vriable or you can say constants
+env.config();
+
+//mongodb connection
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.fmpxm.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() => {
+    console.log('Database connected');
+});
+
+
+//middleware for passing post data
+// app.use(express.json());
+app.use(cors());
+app.use(express.json());  
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
+app.use('/api', userRoutes);
+app.use('/api', adminRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes); 
+app.use('/api', cartRoutes);
+app.use('/api', initialDataRoutes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+
